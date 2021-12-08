d4f12dc65bcfa73f4d1d7808054a943b373c7b13
Ryan Jones <rjones4721@gmail.com>
2021-12-08 14:50:17 +0000

    initial commit

---
+++ Assets/Scripts/LightingPreset.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName ="Lighting Preset", menuName ="Scriptables/Lighting Preset", order =1)]
+public class LightingPreset : ScriptableObject
+{
+	public Gradient AmbientColour;
+	public Gradient DirectionalColour;
+	public Gradient FogColour;
+}
---
+++ Assets/Scripts/MoonControl.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonControl : MonoBehaviour
+{
+
+    public float distance = 1000.0f;
+    public float scale = 15.0f;
+
+    void Start()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, distance);
+        transform.localScale = new Vector3(scale, scale, scale);
+    }
+}
